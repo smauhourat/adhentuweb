0a7a1e36611ca7aae45bca3571bc9a7d27071718
SMauhourat <santiagomauhourat@hotmail.com>
2021-09-19 08:07:37 -0300

    varios

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -1,5 +1,8 @@
 <?php
 
+require("class.phpmailer.php");
+require("class.smtp.php");
+
 $errorMSG = "";
 
 // NAME
@@ -32,8 +35,14 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "email@gmail.com";
-$Subject = "New Message Received";
+$EmailTo = "info@adhentux.com";
+$Subject = "Contacto Web - adhentux.com";
+
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "c2360683.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "info@adhentux.com.ar";  // Mi cuenta de correo
+$smtpClave = "Catal1na//";  // Mi contraseña
+
 
 // prepare email body text
 $Body = "";
@@ -50,15 +59,49 @@ $Body .= "Message: ";
 $Body .= $message;
 $Body .= "\n";
 
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 465; 
+$mail->SMTPSecure = 'ssl';
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+// VALORES A MODIFICAR //
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $name;
+$mail->AddAddress($EmailTo); // Esta es la dirección a donde enviamos los datos del formulario
+
+//$mail->Subject = "Adhentux.com - Contacto Web"; // Este es el titulo del email.
+//$mensajeHtml = nl2br($Body);
+//$mail->Body = "{$mensajeHtml} <br /><br />{$name}<br />{$email}"; // Texto del email en formato HTML
+//$mail->AltBody = "{$Body} \n\n {$name} \n {$email}"; // Texto sin formato <HTML></HTML>
+
+$mail->Subject = "Adhentux.com - Contacto Web"; // Este es el titulo del email.
+$mensajeHtml = "Cuerpo";
+$mail->Body = "{$mensajeHtml}"; // Texto del email en formato HTML
+$mail->AltBody = "{$Body}"; // Texto sin formato HTML
+
+
+// FIN - VALORES A MODIFICAR //
+
+$success = $mail->Send(); 
+
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+//$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+//echo $mensajeHtml;
+
 
 // redirect to success page
 if ($success && $errorMSG == ""){
    echo "success";
 }else{
     if($errorMSG == ""){
-        echo "Something went wrong :(";
+        echo "Ha ocurrido un error :(";
     } else {
         echo $errorMSG;
     }
