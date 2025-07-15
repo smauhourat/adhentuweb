35cdfb7b5c1496144b41870780747c038784d4a3
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2025-07-15 17:57:53 -0300

    faqs end

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,20 +1,20 @@
-(function($) {
-  
-  "use strict";  
+(function ($) {
 
-  $(window).on('load', function() {
+  "use strict";
 
-  /*Page Loader active
-  ========================================================*/
-  $('#preloader').fadeOut();
+  $(window).on('load', function () {
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 50) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
+    /*Page Loader active
+    ========================================================*/
+    $('#preloader').fadeOut();
+
+    // Sticky Nav
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 50) {
+        $('.scrolling-navbar').addClass('top-nav-collapse');
+      } else {
+        $('.scrolling-navbar').removeClass('top-nav-collapse');
+      }
     });
 
     // one page navigation 
@@ -25,93 +25,93 @@
     /* Auto Close Responsive Navbar on Click
     ========================================================*/
     function close_toggle() {
-        if ($(window).width() <= 768) {
-            $('.navbar-collapse a').on('click', function () {
-                $('.navbar-collapse').collapse('hide');
-            });
-        }
-        else {
-            $('.navbar .navbar-inverse a').off('click');
-        }
+      if ($(window).width() <= 768) {
+        $('.navbar-collapse a').on('click', function () {
+          $('.navbar-collapse').collapse('hide');
+        });
+      }
+      else {
+        $('.navbar .navbar-inverse a').off('click');
+      }
     }
     close_toggle();
     $(window).resize(close_toggle);
 
     /* WOW Scroll Spy
     ========================================================*/
-     var wow = new WOW({
+    var wow = new WOW({
       //disabled for mobile
-        mobile: false
+      mobile: false
     });
 
-    wow.init();    
+    wow.init();
 
-     /* Testimonials Carousel 
-    ========================================================*/
+    /* Testimonials Carousel 
+   ========================================================*/
     var owl = $("#testimonials");
-      owl.owlCarousel({
-        items:7,
-        loop: true,
-        nav: false,
-        dots: true,
-        center: true,
-        margin: 15,
-        slideSpeed: 1000,
-        stopOnHover: true,
-        autoplay: true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:false,
-        responsiveClass: true,
-        responsiveRefreshRate: true,
-        responsive : {
-            0 : {
-                items: 1
-            },
-            768 : {
-                items: 2
-            },
-            960 : {
-                items: 3
-            },
-            1200 : {
-                items: 3
-            },
-            1920 : {
-                items: 3
-            }
+    owl.owlCarousel({
+      items: 7,
+      loop: true,
+      nav: false,
+      dots: true,
+      center: true,
+      margin: 15,
+      slideSpeed: 1000,
+      stopOnHover: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: false,
+      responsiveClass: true,
+      responsiveRefreshRate: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        960: {
+          items: 3
+        },
+        1200: {
+          items: 3
+        },
+        1920: {
+          items: 3
         }
-      });  
-      
+      }
+    });
+
 
     /* Back Top Link active
     ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+    $('.back-to-top').on('click', function (event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    });
 
-  });   
-  
+  });
+
   // ========================================================================= //
   //  Typed Js
   // ========================================================================= //
 
   var typed = $(".typed");
 
-  $(function() {
+  $(function () {
     var strings = $('.typed-items').text();
     strings = $('.typed-items').data('typed-person') + ',' + strings;
     strings = strings.split(',');
@@ -120,6 +120,15 @@
       strings: strings,
       typeSpeed: 40,
       loop: true,
+    });
+  });
+
+  /**
+* Frequently Asked Questions Toggle
+*/
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
     });
   });
 
